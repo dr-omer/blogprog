7a39bbdd5487a18c990fe2e4d924e7b220dc5b19
omergilani <omergilani@users.noreply.github.com>
2022-05-05 09:16:45 +0000

    deploy: a55f4d48be00e0608af99d31d53dee6b1cd6b410

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/blogprog/assets/js/search-data.json', true);
+    request.open('GET', '/prog4cm/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
